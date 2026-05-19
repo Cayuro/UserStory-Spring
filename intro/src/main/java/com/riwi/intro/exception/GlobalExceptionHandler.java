352d7cf23d2f93a02784a4b5a30cb89a30a4038e
CarlosDcastano <carlosdueiner1996@gmail.com>
2026-05-19 08:47:11 -0500

    feature repository with JPA: changing the repositories, the implements are not needed, only the interface that extends of the JPA repository and use the methods, and is possible to create specific methods

---
+++ intro/src/main/java/com/riwi/intro/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.riwi.intro.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleResourceNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
+        return new ApiError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+    }
+}
