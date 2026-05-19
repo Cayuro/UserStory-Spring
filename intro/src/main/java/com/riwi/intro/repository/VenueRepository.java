c6876c10a0000267ebdf04049df0f49fcfc427cc
CarlosDcastano <carlosdueiner1996@gmail.com>
2026-05-19 09:06:28 -0500

    making the venue repository with the posibility of a search only with a slice of the name, and without casesensitive

--- intro/src/main/java/com/riwi/intro/repository/VenueRepository.java
+++ intro/src/main/java/com/riwi/intro/repository/VenueRepository.java
@@ -1,11 +1,11 @@
 package com.riwi.intro.repository;
 
 import com.riwi.intro.models.Venue;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface VenueRepository extends JpaRepository<Venue, Integer> {
 
-    List<Venue> findByNameContainingIgnoreCase(String name);
+    Page<Venue> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
